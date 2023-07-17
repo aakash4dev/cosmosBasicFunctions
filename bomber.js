ea751f72203007c26a66dde9c8489391efb41430
aakash4dev <aakash4dev.crypto@gmail.com>
2023-07-17 14:30:12 +0530

    bomber updated

--- bomber.js
+++ bomber.js
@@ -1,13 +1,17 @@
 const { exec } = require('child_process');
 
 console.log('Starting shell script execution');
+
+chainname = "test0001"
+address1 = "cosmos1qac3s3law0n8ck4rmtcezcs5we8des97pg3550" // alice
+address2 = "cosmos1nt3afyt8wy78ltc6cj232045pmugttepcgmzrx" // bob
+
 function executeShellScript() {
-  const goto_blockchain_directory = "cd test0001"
-  const command = 'test0001d tx bank send cosmos1qac3s3law0n8ck4rmtcezcs5we8des97pg3550 cosmos1nt3afyt8wy78ltc6cj232045pmugttepcgmzrx 10stack -y';
+  const command = `cd ${chainname}; ${chainname}d tx bank send ${address1} ${address2} 10stack -y`;
   
   exec(command, (error, stdout, stderr) => {
     if (error) {
-      console.error(`Error executing shell script: ${error.message}`);
+      console.error(`Error executing shell script: ${error.message}`); 
       return;
     }
     
